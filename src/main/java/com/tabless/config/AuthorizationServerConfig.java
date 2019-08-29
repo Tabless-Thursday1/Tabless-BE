69d84056283426e2db4d7b477d93da1bbf8617da
Vaeliwen <zaman@imbriaco.com>
2019-08-28 20:09:59 -0400

    FIXED Auth.

--- src/main/java/com/tabless/config/AuthorizationServerConfig.java
+++ src/main/java/com/tabless/config/AuthorizationServerConfig.java
@@ -12,10 +12,10 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter
 {
-    static final String CLIENT_ID = System.getenv("OAUTHCLIENTID"); // read from environment variable
-    static final String CLIENT_SECRET = System.getenv("OAUTHCLIENTSECRET"); // read from environment variable
-    // static final String CLIENT_ID = "lambda-client";
-    // static final String CLIENT_SECRET = "lambda-secret";
+    //    static final String CLIENT_ID = System.getenv("OAUTHCLIENTID"); // read from environment variable
+//    static final String CLIENT_SECRET = System.getenv("OAUTHCLIENTSECRET"); // read from environment variable
+    static final String CLIENT_ID = "lambda-client";
+    static final String CLIENT_SECRET = "lambda-secret";
     static final String GRANT_TYPE_PASSWORD = "password";
     static final String AUTHORIZATION_CODE = "authorization_code";
     static final String REFRESH_TOKEN = "refresh_token";
@@ -32,15 +32,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private PasswordEncoder encoder;
     @Override
-    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception
-    {
+    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
         //                .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT)
-        // configurer.inMemory().withClient(CLIENT_ID).secret(encoder.encode(CLIENT_SECRET)).authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, IMPLICIT).scopes(SCOPE_READ, SCOPE_WRITE, TRUST).accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+        configurer.inMemory()
+                .withClient(CLIENT_ID)
+                .secret(encoder.encode(CLIENT_SECRET))
+                .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, IMPLICIT)
+                .scopes(SCOPE_READ, SCOPE_WRITE, TRUST)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS)
+                .refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
     }
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception
-    {
-        endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore)
+                .authenticationManager(authenticationManager);
         endpoints.pathMapping("/oauth/token", "/login");
     }
-}
